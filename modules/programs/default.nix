61c77fd6df85db8832efa18113624d1023dca4e3
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2025-04-09 02:06:26 -0500

    Adds Rofi Program

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,7 +1,8 @@
-{ pkgs, env, ... }:
+{ config, pkgs, env, ... }:
 
 {
   imports = [
+    (import ./rofi { inherit config pkgs; })
     (import ./zsh { inherit pkgs; })
     (import ./git { inherit pkgs env; })
   ];
