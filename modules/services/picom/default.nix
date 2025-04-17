36615c890451d8ad48014ed8365c3ae3776cb89b
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2025-04-17 02:22:18 -0500

    Fix Tearing: Needs Accelerate 3D Graphics

--- modules/services/picom/default.nix
+++ modules/services/picom/default.nix
@@ -5,6 +5,12 @@
     enable = true;
     #backend = "glx";
     backend = "xrender";
+
+    extraArgs = [
+      "--xrender-sync-fence false"  # Desactiva xrender-sync-fence
+      "--use-damage false"  # Desactiva el uso de damage
+    ];
+
     fade = true;
     fadeSteps = [ 0.03 0.03 ];
 
