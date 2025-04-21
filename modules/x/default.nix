8e2ba85dec26f06123166685e222c382d53fca52
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2025-04-20 23:46:05 -0500

    Fix resolution and wallpaper size

--- modules/x/default.nix
+++ modules/x/default.nix
@@ -12,6 +12,11 @@
     export DISPLAY=:0
     export XAUTHORITY="${config.home.homeDirectory}/.Xauthority"
     dbus-update-activation-environment --systemd DISPLAY XAUTHORITY
+
+    # VMware
+    if command -v vmware-user-suid-wrapper >/dev/null; then
+      vmware-user-suid-wrapper &
+    fi
   '';
 
   xsession = {
