af9a481d75e1fd1bdf86bbc467963c87fac59b23
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2025-05-26 22:07:37 -0500

    [REFACTOR] x: Move .xinitrc content into a dedicated file
    Fixes #2

--- modules/x/default.nix
+++ modules/x/default.nix
@@ -1,23 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.file.".xinitrc".text = ''
-    #!/bin/sh
-    # /etc/X11/xinit/xinitrc
-    
-    # global xinitrc file, used by all X sessions started by xinit (startx)
-    # invoke global X session script
-    . /etc/X11/Xsession
-
-    export DISPLAY=:0
-    export XAUTHORITY="${config.home.homeDirectory}/.Xauthority"
-    /usr/bin/dbus-update-activation-environment --systemd DISPLAY XAUTHORITY
-
-    # VMware
-    if command -v vmware-user-suid-wrapper >/dev/null; then
-      /usr/bin/vmware-user-suid-wrapper &
-    fi
-  '';
+  home.file.".xinitrc".text = builtins.replaceStrings
+    [ "{{homeDirectory}}" ]
+    [ config.home.homeDirectory ]
+    (builtins.readFile ./xinitrc);
 
   xsession = {
     enable = true;
