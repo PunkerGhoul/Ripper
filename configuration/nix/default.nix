dc50be4a5443bb5f3cd397a3ebaccf338ff35d61
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2025-08-09 17:51:59 -0500

    [Feat] Programs: Add LibreWolf browser with dual profiles
    - Updates NixPkgs and Home Manager channels

--- configuration/nix/default.nix
+++ configuration/nix/default.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 
 {
+  # Enable NUR (Nix User Repository) for additional packages
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+      };
+    };
+    # Allow unfree packages (needed for some browser extensions)
+    allowUnfree = true;
+  };
+
   nix = {
     package = pkgs.nix;
     settings = {
