9efde35e5cb4162c6d798c29b54b0f238776a55d
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2026-03-08 23:33:16 -0500

    [Feat] Update zsh configuration to inherit home directory for dotDir

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -4,7 +4,7 @@
   imports = [
     (import ./rofi { inherit config pkgs; })
     (import ./kitty { inherit config pkgs unstable; })
-    (import ./zsh { inherit pkgs; })
+    (import ./zsh { inherit config pkgs; })
     (import ./git { inherit pkgs env; })
     (import ./bat { inherit pkgs; })
     (import ./jq { inherit pkgs; })
