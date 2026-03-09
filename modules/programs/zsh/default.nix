a0bc0e349d6d6621783fff1331eca6959668b10d
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2026-03-08 23:33:16 -0500

    [Feat] Update zsh configuration to inherit home directory for dotDir

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ config, pkgs, ...}:
 
 let
   ohMyZshConfig = import ./oh-my-zsh { inherit pkgs; };
@@ -6,7 +6,7 @@ let
 in {
   programs.zsh = {
     enable = true;
-    dotDir = ".config/zsh";
+    dotDir = "${config.home.homeDirectory}/.config/zsh";
     profileExtra = ''
       if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ] && [ "$(tty)" = "/dev/tty1" ]; then
         exec /usr/bin/startx ${pkgs.i3}/bin/i3
