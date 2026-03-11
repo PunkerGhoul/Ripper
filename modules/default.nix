e862126bce966485c909d4e38a6cba8ac8127263
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2026-03-10 23:39:51 -0500

    [Fix] Update audio service import to include lib dependency

--- modules/default.nix
+++ modules/default.nix
@@ -4,6 +4,6 @@
   imports = [
     (import ./x { inherit config pkgs lib; })
     (import ./programs { inherit config pkgs lib unstable env; })
-    (import ./services { inherit config pkgs; })
+    (import ./services { inherit config pkgs lib; })
   ];
 }
