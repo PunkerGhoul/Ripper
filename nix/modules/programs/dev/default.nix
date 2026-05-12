713482e7feed7c2064b44209c91421f3113fd1be
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2026-05-12 03:17:59 -0500

    196

--- nix/modules/programs/dev/default.nix
+++ nix/modules/programs/dev/default.nix
@@ -8,7 +8,12 @@
 
   options.ripper.programs.dev.packages = lib.mkOption {
     type = with lib.types; listOf package;
-    default = [ pkgs.go ];
+    default = [
+        pkgs.go
+        pkgs.cargo
+        pkgs.gcc
+        pkgs.clang
+    ];
     description = "Packages provided by dev program modules (python/nodejs).";
   };
 }
