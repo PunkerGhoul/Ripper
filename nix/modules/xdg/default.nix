a201ddf97dfbe5f925628abe4eac61005f0df01b
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2026-05-22 00:49:55 -0500

    [Feat] Add XDG module

---
+++ nix/modules/xdg/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+	xdg = {
+		enable = true;
+		localBinInPath = true;
+
+		autostart.enable = true;
+		mime.enable = true;
+
+		userDirs = {
+			enable = true;
+			createDirectories = true;
+            setSessionVariables = false;
+		};
+
+		portal = {
+			enable = true;
+			config = {
+				common = {
+					default = [ "gtk" ];
+				};
+			};
+			extraPortals = [
+				pkgs.xdg-desktop-portal-gtk
+			];
+		};
+	};
+}
