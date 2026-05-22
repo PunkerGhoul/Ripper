20b2cf177930d8bbf40f6e01e65437a8d713b943
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2026-05-22 00:49:42 -0500

    [Feat] Add utility program module registry

---
+++ nix/modules/programs/utilities/default.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, lib, unstable, nixosPkgs, env, nixGLCommand, ... }:
+
+{
+  imports = [
+    (import ./bat { inherit pkgs; })
+    (import ./codex { inherit nixosPkgs; })
+    (import ./git { inherit pkgs env; })
+    (import ./jq { inherit pkgs; })
+    (import ./fzf { inherit pkgs; })
+    (import ./kitty { inherit config pkgs unstable nixGLCommand; })
+    (import ./neovim { inherit pkgs unstable; })
+    (import ./rofi { inherit config pkgs; })
+    (import ./tmux { inherit pkgs; })
+    (import ./zsh { inherit config pkgs lib; })
+  ];
+
+  config.ripper.programs.packages = [
+    pkgs.curl
+    pkgs.cron
+    pkgs.wget
+    pkgs.ripgrep
+    pkgs.ffmpeg
+    pkgs.uv
+    pkgs.netcat-gnu
+    pkgs.socat
+    pkgs.inetutils
+    pkgs.strace
+    # Sage is intentionally sourced from nixosPkgs; switch to pkgs.sage to use the base nixpkgs input.
+    # The install check override works around Sage doctest failures seen on some nixos-unstable revisions.
+    (nixosPkgs.sage.overrideAttrs (_: {
+      doInstallCheck = false;
+    }))
+    pkgs.logseq
+    pkgs.openvpn
+    pkgs.xxd
+    pkgs.xcd
+    pkgs.xdg-utils
+    pkgs.xclip
+    pkgs.tree
+    pkgs.unzip
+    pkgs.rsync
+    pkgs.zip
+    pkgs.gnutar
+    pkgs.gzip
+    pkgs.basez
+  ];
+
+}
